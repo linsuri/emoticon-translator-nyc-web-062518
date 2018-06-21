feb422441bc22574bc8ea95e7c5f9fb0bdb6f485
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 17:38:14 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   library = {"get_meaning" => {}, "get_emoticon" => {}}
   YAML.load_file(file_path).each do |translation, emoticons_array|
     english, japanese = emoticons_array
-    library["get_meaning"][japanese] = english
+    library["get_meaning"][japanese] = translation
     library["get_emoticon"][english] = japanese
   end  
   library  
